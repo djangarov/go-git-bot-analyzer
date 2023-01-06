dd33bd8a949505cc8c704d9057060d7f3949ec66
Ivelin Dzhangarov <djangarov.i@gmail.com>
2023-01-06 23:46:06 +0200

    Add gitlab host suffix

--- analyzer.go
+++ analyzer.go
@@ -3,16 +3,27 @@ package main
 import (
 	"flag"
 	"fmt"
+	"strings"
+
+	"github.com/djangarov/go-git-bot-analyzer/utils"
 )
 
 func main() {
-	hostFlag := flag.String("gitlab-host", "https://gitlab.com", "provide the gitlab host of your repo")
-	portFlag := flag.Int("port", 8080, "provide the port that the gitlab bot must run on")
-	privateTokenFlag := flag.String("private-token", "", "provide the token of your gitlab account")
+	host := flag.String("gitlab-host", "https://gitlab.com", "provide the gitlab host of your repo")
+	port := flag.Int("port", 8080, "provide the port that the gitlab bot must run on")
+	privateToken := flag.String("private-token", "", "provide the token of your gitlab account")
 
 	flag.Parse()
 
-	fmt.Println(*hostFlag)
-	fmt.Println(*portFlag)
-	fmt.Print(*privateTokenFlag)
+	// if the provided host doesn't end with '/', append '/'
+	if strings.LastIndex(*host, utils.URL_SLASH) != len(*host)-1 {
+		*host += utils.URL_SLASH
+	}
+
+	// append gitlab transmitter prefix transmitter/v4/projects
+	*host += utils.GITLAB_API_PREFIX
+
+	fmt.Println(*host)
+	fmt.Println(*port)
+	fmt.Print(*privateToken)
 }
---
+++ utils/constsants.go
@@ -0,0 +1,6 @@
+package utils
+
+const (
+	URL_SLASH         = "/"
+	GITLAB_API_PREFIX = "api/v4/projects/"
+)
